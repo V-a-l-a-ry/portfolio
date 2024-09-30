5bb4b6f4f3f096e88bf16638f53d23f0319aa71f
V-a-l-a-ry <inondavalary@gmail.com>
2024-09-30 10:43:52 +0300

    redirection

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const btn =document.getElementById('menu-btn')
 const nav =document .getElementById('menu')
+const isVisble =false
 
 btn.addEventListener('click',()=>{
     btn.classList.toggle('open')
